a909d340914fe9184397445cd0b12455afbce3f3
DESKTOP-6UJNEHM\Rain <rainal14@qq.com>
2020-09-27 01:23:21 +0800

    实验三：添加隐式intent（活动响应intent）

--- Lab_03/Lab003/app/src/main/java/com/example/lab003/TestIntentActivity.java
+++ Lab_03/Lab003/app/src/main/java/com/example/lab003/TestIntentActivity.java
@@ -2,7 +2,11 @@ package com.example.lab003;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TestIntentActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class TestIntentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_intent);
+
+        Button button1 = findViewById(R.id.button_baidu);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.baidu.com"));
+                startActivity(intent);
+            }
+        });
     }
 }
