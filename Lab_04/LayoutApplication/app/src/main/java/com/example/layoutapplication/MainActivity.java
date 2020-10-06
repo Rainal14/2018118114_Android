586408c85f620af836a8d4f36885c263e569d6f1
DESKTOP-6UJNEHM\Rain <rainal14@qq.com>
2020-10-06 13:10:49 +0800

    实验四：布局：更新editText、Dialog

--- Lab_04/LayoutApplication/app/src/main/java/com/example/layoutapplication/MainActivity.java
+++ Lab_04/LayoutApplication/app/src/main/java/com/example/layoutapplication/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.layoutapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText editText = findViewById(R.id.edit_text);
+
+        Button button1 = findViewById(R.id.display_button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String inputText = editText.getText().toString();
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("这是一个对话框");
+                dialog.setMessage(inputText);
+                dialog.setCancelable(false);
+                dialog.setPositiveButton("好的", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
+                dialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
+                dialog.show();
+            }
+        });
     }
 }
