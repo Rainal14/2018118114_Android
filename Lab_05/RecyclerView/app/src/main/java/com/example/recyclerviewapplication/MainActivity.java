c096580d8983aba21597aa4c5a4942b55acd5055
DESKTOP-6UJNEHM\Rain <rainal14@qq.com>
2020-10-09 22:11:49 +0800

    实验五：RecyclerView：更新listview

--- Lab_05/RecyclerView/app/src/main/java/com/example/recyclerviewapplication/MainActivity.java
+++ Lab_05/RecyclerView/app/src/main/java/com/example/recyclerviewapplication/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.recyclerviewapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] data = {"Dog", "Cat", "Ant", "Tiger", "Lion", "Pig", "Bird", "Horse", "Elephant", "Panda", "Duck", "Man", "Girl", "Ultraman"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, data);
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
     }
 }
